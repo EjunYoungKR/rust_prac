2053c77fab4acd982dbe10c1c81f05db75c9df22
EjnuYoungKR <junyeong6877@gmail.com>
2023-01-06 21:02:28 +0900

    22.1.6_1

---
+++ chapter2/target/debug/chapter2.d
@@ -0,0 +1,1 @@
+/Users/junyoung_lee/rust_prac/chapter2/target/debug/chapter2: /Users/junyoung_lee/rust_prac/chapter2/src/main.rs
---
+++ chapter2/target/debug/deps/chapter2-9052f3a2af266be7.d
@@ -0,0 +1,5 @@
+/Users/junyoung_lee/rust_prac/chapter2/target/debug/deps/chapter2-9052f3a2af266be7: src/main.rs
+
+/Users/junyoung_lee/rust_prac/chapter2/target/debug/deps/chapter2-9052f3a2af266be7.d: src/main.rs
+
+src/main.rs:
