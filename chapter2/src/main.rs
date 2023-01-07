fc71d647295cc4f087b588027a4b9de8a90baf1b
EjnuYoungKR <junyeong6877@gmail.com>
2023-01-07 11:46:24 +0900

    new update

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -1,10 +1,47 @@
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+	
+	let mut x =5;
+	let mut y;
+	let mut z;
 
-    let (x,y,z) = tup;
+	println!("x is {}", x);
+	
+	y = 2.1;
+	z = -1;
+
+	println!("y is {}",y);
+	println!("z is {}",z);
+
+
+
+	let spaces = "   ";
+	let spaces = spaces.len();
+
+	println!("The value of spaces is {}",spaces);
+
+
+
+    let tup:(u16, f64, i8) = (500, -3.3, -3);
+
+    (x,y,z) = tup;
 
     println!("The value of x is : {}", x);
     println!("The value of y is : {}", y);
     println!("The value of z is : {}", z);
+
+	x = 3;
+
+	println!("{}",x);
+
+
+	let arr1:[i16;2] = [500, 455];
+
+	let arr_r:[f64;2] = [3.4, 1.1];
+
+	let arr = [0;3];
+
+	println!("{}",arr1[0]);
+	println!("{}",arr_r[1]);
+	println!("{}",arr[0]);
 
 }
